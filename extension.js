38e91e972b7cf692d479ec06d8e5bf2019b58d39
ridwanmahendra <ridwanmahenra@teknokrat.ac.id>
2023-03-26 04:20:45 +0700

    add logistic regression

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const fs = require('fs');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,21 +18,42 @@ function activate(context) {
 		const message = 'Hello, MKA-UGM';
 		vscode.window.showInformationMessage(message);
 	});
-
 	context.subscriptions.push(disposable);
 
-	let disposable1 = vscode.commands.registerCommand('mkalib.logisticRegression', function () {
-		vscode.window.showInformationMessage('Logistic Regression Coming Soon');
+
+	const log_res = vscode.commands.registerCommand('mkalib.logisticRegression', function () {
+		vscode.window.showQuickPick(["Logistic Regression"], {placeHolder: "Select Model Type"}).then(function (type) {
+		  if (!type) return;
+		  vscode.window.showQuickPick(["from scratch", "with sklearn"], {placeHolder: "Select Implementation Type for " + type}).then(function (implementation) {
+			if (!implementation) return;
+			let fileName = type.toLowerCase().replace(" ", "_") + "_" + implementation.toLowerCase().replace(" ", "_") + ".py";
+			let templatePath = path.join(context.extensionPath, 'code_template', fileName);
+			fs.readFile(templatePath, function (err, data) {
+			  if (err) {
+				vscode.window.showErrorMessage("Unable to load template file: " + err);
+				return;
+			  }
+			  let filePath = path.join(vscode.workspace.rootPath, fileName);
+			  fs.writeFile(filePath, data, function (err) {
+				if (err) {
+				  vscode.window.showErrorMessage("Unable to create file: " + err);
+				  return;
+				}
+				vscode.window.showInformationMessage("File " + fileName + " created successfully!");
+			  });
+			});
+		  });
+		});
 	  });
 	
-	  context.subscriptions.push(disposable1);
+	  context.subscriptions.push(log_res);
+
 	
 	  // Fitur "Say linear regression!"
-	  let disposable2 = vscode.commands.registerCommand('mkalib.linearRegression', function () {
+	let disposable2 = vscode.commands.registerCommand('mkalib.linearRegression', function () {
 		vscode.window.showInformationMessage('Linear Regression Coming Soon!');
-	  });
-	
-	  context.subscriptions.push(disposable2);
+	});
+	context.subscriptions.push(disposable2);
 
 	// Fitur bikin file baru
 	let createFile = vscode.commands.registerCommand('mkalib.createFile', function () {
